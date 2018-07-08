6abada6d44eaf21f6296675b22cba80d9720c980
Nikolas Omelianov <nikolas@Nikolass-Mac-mini.local>
2018-07-08 19:53:38 +0300

    init project

---
+++ NotificationsFirst/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  NotificationsFirst
+//
+//  Created by Nikolas Omelianov on 07.07.2018.
+//  Copyright © 2018 Nikolas Omelianov. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    var pizzaNumber = 0
+    let pizzaSteps = ["Make Pizza","Roll Dough","Add Sause","Add Cheese","Add Ingrefients","Bake ","Done"]
+    func updatePizzaStep(request: UNNotificationRequest){
+        if request.identifier.hasPrefix("message.pizza") {
+            var stepNumber = request.content.userInfo["step"] as! Int
+            stepNumber = (stepNumber + 1) % pizzaSteps.count
+            let updatedContent = makePizzaContent()
+            updatedContent.body = pizzaSteps[stepNumber]
+            updatedContent.userInfo["step"] = stepNumber
+            updatedContent.subtitle = request.content.subtitle
+            addNotification(trigger: request.trigger, content: updatedContent, identifier: request.identifier)
+        }
+    }
+    var isGrantedNotificationAccess = false
+    func makePizzaContent() -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "A Timed Piza Step"
+        content.body = "Making Pizza"
+        content.userInfo = ["step": 0]
+        return content
+    }
+    func addNotification(trigger : UNNotificationTrigger?, content: UNMutableNotificationContent, identifier: String) {
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) {
+            (error) in
+            if error != nil {
+                print("error adding notification:\(error?.localizedDescription ?? "whoops")")
+            }
+        }
+    }
+    
+    @IBAction func Scedule(_ sender: UIButton) {
+        if isGrantedNotificationAccess{
+            let content = UNMutableNotificationContent()
+            content.title = "A sceduled pizza"
+            content.body = "time to make a Pizza"
+            let unitFlags: Set<Calendar.Component> = [.minute,.hour,.second]
+            var date = Calendar.current.dateComponents(unitFlags, from: Date())
+            date.second = date.second! + 15
+            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+            addNotification(trigger: trigger, content: content, identifier: "message.scedule")
+            
+        }
+    }
+    
+    @IBAction func Make(_ sender: UIButton) {
+        if isGrantedNotificationAccess{
+            let content = makePizzaContent()
+            pizzaNumber += 1
+            content.subtitle = "Pizza \(pizzaNumber)"
+            
+//            let trigget = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+            let trigget = UNTimeIntervalNotificationTrigger(timeInterval: 60.0, repeats: true)
+            addNotification(trigger: trigget, content: content, identifier: "message.pizza.\(pizzaNumber)")
+        }
+    }
+    
+    @IBAction func Next(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests
+            { (requests) in
+                if let request = requests.first {
+                    if request.identifier.hasPrefix("message.pizza"){
+                        self.updatePizzaStep(request: request)
+                    } else {
+                        let content = request.content.mutableCopy() as! UNMutableNotificationContent
+                        self.addNotification(trigger: request.trigger, content: content, identifier: request.identifier)
+                    }
+                }
+            }
+    }
+    
+    @IBAction func Pending(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests{(requestList) in
+                print("\(Date()) --> \(requestList.count) request pending")
+            for request in requestList {
+                print("\(request.identifier) body:\(request.content.body)")
+            }
+        }
+    }
+    
+    @IBAction func Delivered(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getDeliveredNotifications { (notifications) in
+            print ("\(Date()) ---\(notifications.count) delivered")
+            for notification in notifications {
+                print("\(notification.request.identifier)    \(notification.request.content.body)")
+            }
+        }
+    }
+    
+    @IBAction func Remove(_ sender: UIButton) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests {
+            (requests) in
+            if let reqest = requests.first {
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [reqest.identifier])
+            }
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound,]) { (granted, error) in
+            self.isGrantedNotificationAccess = granted
+            if !granted {
+                //TODO
+            }
+        }
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert,.sound])
+    }
+   
+
+
+}
+
